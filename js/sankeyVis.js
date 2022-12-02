859cbefbaada4b2b74da91a83d4a9c8a3103d0cf
viyer77 <viyer77@gmail.com>
2022-12-01 21:11:17 -0600

    Updated sizing of the heatmap panel and sankey so that they show in same screen (almost)
    Updated sizing of the heatmap panel and sankey so that they show in same screen (almost)

--- js/sankeyVis.js
+++ js/sankeyVis.js
@@ -30,7 +30,8 @@ class SankeyVis {
 		let vis = this;
 
         // set margins
-		vis.margin = {top: 40, right: 40, bottom: 50, left: 40};
+		//vis.margin = {top: 40, right: 40, bottom: 50, left: 40};
+        vis.margin = {top: 10, right: 10, bottom: 10, left: 10};
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
         vis.colors = ["#5E4FA2", "#3288BD", "#66C2A5", "#ABDDA4", "#E6F598", 
@@ -48,8 +49,10 @@ class SankeyVis {
 
         // Set the sankey diagram properties
         vis.sankey = d3.sankey()
-            .nodeWidth(36)
-            .nodePadding(40)
+            .nodeWidth(120)
+            .nodePadding(60)
+            //.nodeWidth(36)
+            //.nodePadding(40)
             .size([vis.width, vis.height]);
 
         // Set path group
