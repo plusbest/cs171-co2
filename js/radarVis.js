47a32c522f09be36a4006e58d5e5a5bd31005a3b
plusbest <jerry.wycoco@gmail.com>
2022-12-04 17:14:42 -0500

    icon update....again

--- js/radarVis.js
+++ js/radarVis.js
@@ -186,17 +186,17 @@ class RadarVis {
         // Return the correct icon based on param
         switch(field){
             case "percent_oil_co2":
-                return "../static/icons/oil.png";
+                return "static/icons/oil.png";
             case "percent_coal_co2":
-                return "../static/icons/coal.png";
+                return "static/icons/coal.png";
             case "percent_gas_co2":
-                return "../static/icons/propane.png";
+                return "static/icons/propane.png";
             case "percent_flaring_co2":
-                return "../static/icons/flaring.png";
+                return "static/icons/flaring.png";
             case "percent_cement_co2":
-                return "../static/icons/cement.png";
+                return "static/icons/cement.png";
             case "percent_other_co2":
-                return "../static/icons/other-energy.png";
+                return "static/icons/other-energy.png";
             default:
                 throw new Error("Invalid field passed into radarVis.fieldToIconPath");
         }
@@ -385,27 +385,27 @@ class RadarVis {
                         <div style="border: thin solid grey; border-radius: 5px; background: lightgrey; padding: 20px">
                             <h3>${ d.country }</h3>
                             <h4>
-                               <img src="/static/icons/coal.png" height="24" width="24" alt="coal">
+                               <img src="../static/icons/coal.png" height="24" width="24" alt="coal">
                                <strong>Coal:</strong> ${ Math.round(d.percent_coal_co2 * 100) / 100 }%
                             </h4>
                             <h4>
-                               <img src="/static/icons/oil.png" height="24" width="24" alt="coal">
+                               <img src="../static/icons/oil.png" height="24" width="24" alt="coal">
                                <strong>Oil:</strong> ${ Math.round(d.percent_oil_co2 * 100) / 100 }%
                             </h4>
                             <h4>
-                               <img src="/static/icons/propane.png" height="24" width="24" alt="coal">
+                               <img src="../static/icons/propane.png" height="24" width="24" alt="coal">
                                <strong>Gas:</strong> ${ Math.round(d.percent_gas_co2 * 100) / 100 }%
                             </h4>
                             <h4>
-                               <img src="/static/icons/cement.png" height="24" width="24" alt="coal">
+                               <img src="../static/icons/cement.png" height="24" width="24" alt="coal">
                                <strong>Cement:</strong> ${ Math.round(d.percent_cement_co2 * 100) / 100 }%
                             </h4>
                             <h4>
-                               <img src="/static/icons/flaring.png" height="24" width="24" alt="coal">
+                               <img src="../static/icons/flaring.png" height="24" width="24" alt="coal">
                                <strong>Flaring:</strong> ${ Math.round(d.percent_flaring_co2 * 100) / 100 }%
                             </h4>
                             <h4>
-                               <img src="/static/icons/other-energy.png" height="24" width="24" alt="coal">
+                               <img src="../static/icons/other-energy.png" height="24" width="24" alt="coal">
                                <strong>Other:</strong> ${ Math.round(d.percent_other_co2 * 100) / 100 }%
                             </h4>
                         </div>`
